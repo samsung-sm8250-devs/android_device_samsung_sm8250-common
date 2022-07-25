username0136/dump_device_xiaomi_sm8250-common

sm8250-common: Initial skeleton
Change-Id: Ic9773001e798552cc4130665a6454fcdaa4ecc72

BoardConfigCommon.mk
@@ -1,0 +1,10 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/xiaomi/sm8250-common
+
+# Inherit the proprietary files
+include vendor/xiaomi/sm8250-common/BoardConfigVendor.mk

kona.mk
@@ -1,0 +1,8 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/xiaomi/sm8250-common/sm8250-common-vendor.mk)

samsung-sm8250-devs/android_device_samsung_sm8250-common

Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

BoardConfigCommon.mk
@@ -1,0 +1,10 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/samsung/sm8250-common
+
+# Inherit the proprietary files
+include vendor/samsung/sm8250-common/BoardConfigVendor.mk

kona.mk
@@ -1,0 +1,8 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/samsung/sm8250-common/sm8250-common-vendor.mk)